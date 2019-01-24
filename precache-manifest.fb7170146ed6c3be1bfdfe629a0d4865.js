eaeec347d46da2ba316a354c32ba2c3568a6cb88
silentmaker <demonmaxc@gmail.com>
2019-01-24 21:17:18 +0800

    Updates

---
+++ precache-manifest.fb7170146ed6c3be1bfdfe629a0d4865.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "93d77c407f4e788f2bd7",
+    "url": "/mr-hat/static/css/main.2a04b062.chunk.css"
+  },
+  {
+    "revision": "93d77c407f4e788f2bd7",
+    "url": "/mr-hat/static/js/main.93d77c40.chunk.js"
+  },
+  {
+    "revision": "b3051add5912e07ee7a7",
+    "url": "/mr-hat/static/js/1.b3051add.chunk.js"
+  },
+  {
+    "revision": "862297c854b5d4cdf704",
+    "url": "/mr-hat/static/js/runtime~main.862297c8.js"
+  },
+  {
+    "revision": "6bcf11cf48e156a412435a79b35e9d5c",
+    "url": "/mr-hat/static/media/hat-3.6bcf11cf.png"
+  },
+  {
+    "revision": "a5c3d008edb71347435fa4cf54db40b2",
+    "url": "/mr-hat/static/media/hat-1.a5c3d008.png"
+  },
+  {
+    "revision": "1ffeba0e2b5b0eacf1dd57fe6a2d50a4",
+    "url": "/mr-hat/static/media/hat-2.1ffeba0e.png"
+  },
+  {
+    "revision": "615f825b0662afe518910c72c42fa319",
+    "url": "/mr-hat/static/media/camera-shutter.615f825b.png"
+  },
+  {
+    "revision": "1a53361ae91684cdb62667b12e48e0ca",
+    "url": "/mr-hat/index.html"
+  }
+];

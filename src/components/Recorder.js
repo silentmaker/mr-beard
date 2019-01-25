aebea48563ccb7e2c93ad5cdc5a2b19083deb40a
silentmaker <demonmaxc@gmail.com>
2019-01-25 14:02:52 +0800

    add screenshot

--- src/components/Recorder.js
+++ src/components/Recorder.js
@@ -12,6 +12,8 @@ class Recorder extends Component {
       isOpen: false,
       isInfo: false,
       isSupported: false,
+      isCapture: false,
+      screenshot: '',
     };
   }
 
@@ -46,7 +48,8 @@ class Recorder extends Component {
   detectFace() {
     if (!this.faceDetector) return;
     this.faceDetector.detect(this.refs.cameraVideo).then((faces) => {
-      const { canvas, ctx } = this;
+      const { canvas, video, ctx } = this;
+      const { isCapture } = this.state;
       const { beard } = this.props;
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       faces.forEach(face => {
@@ -54,7 +57,9 @@ class Recorder extends Component {
         face.landmarks.forEach((landmark) => {
           if (landmark.type === 'mouth') {
             const { x, y } = landmark.locations[0];
+            if (isCapture) ctx.drawImage(video, 0, 0, video.clientWidth, video.clientHeight);
             ctx.drawImage(beardImage, -beard.x * 2, -beard.y * 2, 150, 80, x - 75, y - height / 200 * 80, 150, 80);
+            if (isCapture) this.setState({ isCapture: false, screenshot: canvas.toDataURL('image/png') });
           }
         });
       });
@@ -71,12 +76,22 @@ class Recorder extends Component {
     }
   }
 
+  captureCamera() {
+    this.setState({ isCapture: !this.state.isCapture });    
+  }
+
   toggleInfo() {
     this.setState({ isInfo: !this.state.isInfo });
   }
 
+  clearPhoto() {
+    setTimeout(() => {
+      this.setState({ screenshot: '' });
+    }, 2000);
+  }
+
   render() {
-    const { isOpen, isInfo, isSupported } = this.state;
+    const { isOpen, isInfo, isSupported, screenshot } = this.state;
     return (
       <div className="recorder-container">
         <video className={`camera-video ${isOpen ? 'active' : ''}`} ref="cameraVideo" autoPlay></video>
@@ -85,6 +100,9 @@ class Recorder extends Component {
           <div className="icon-close" onClick={() => this.closeCamera()}></div> :
           <div className="icon-open" onClick={() => this.openCamera()}></div>
         }
+        {!isOpen ? '' :
+          <div className="icon-capture" onClick={() => this.captureCamera()}></div>
+        }
         {isSupported ? '' :
           <div className={`feature-info ${isInfo ? 'active' : ''}`} onClick={() => this.toggleInfo()}>
             <div>Enable FaceDetector</div>
@@ -92,6 +110,9 @@ class Recorder extends Component {
             <div><code>chrome://flags/#enable-experimental-web-platform-features</code></div>
           </div>
         }
+        <a className={`save-btn ${screenshot ? 'active' : ''}`} href={screenshot} download="mr-beard.png">
+          <img src={screenshot} alt="mr-beard.png" onClick={() => this.clearPhoto()}/>
+        </a>
       </div>
     );
   }
